e819ae63518b65eced6abcb371a46eff65eae732
Corey Lutrick <coreylutrick@gmail.com>
2018-05-30 20:32:55 -0700

    styled up the page a little, fixed bootstrap

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -4,13 +4,13 @@ const domString = (weathArr) =>
   weathArr.forEach(info =>
   {
     string += `<div class="row">`;
-    string +=  `<div class='col-md-4 col-md-offset-4 ${info.weather[0].main}'>`;
+    string +=  `<div id='weatherCard' class='col-md-4 col-md-offset-4 ${info.weather[0].main}'>`;
     string +=    `<h2>${info.name}</h2>`;
     string +=    `<p><strong>Current Temperature:</strong> ${info.main.temp}&degF</p>`;
     string +=    `<p><strong>Weather Conditions:</strong> ${info.weather[0].main}</p>`;
     string +=    `<p><strong>Air Pressure:</strong> ${info.main.pressure}</p>`;
     string +=    `<p><strong>Wind Speed:</strong> ${info.wind.speed}</p>`;
-    string +=    `<button id="5day">5 Day Forecast</button>`;
+    string +=    `<button id="fiveDay">5 Day Forecast</button>`;
     string +=  `</div>`;
     string += `</div>`;
   });
