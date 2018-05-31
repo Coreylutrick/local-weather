501c7d369b489cd4490d875503e240e3fc6768c2
Corey Lutrick <coreylutrick@gmail.com>
2018-05-30 18:20:08 -0700

    decided to change domstring for single day

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -6,9 +6,10 @@ const domString = (weathArr) =>
     string += `<div class="row">`;
     string +=  `<div class='col-md-4 col-md-offset-4 ${info.weather[0].main}'>`;
     string +=    `<h2>${info.name}</h2>`;
-    string +=    `<p>${info.weather[0].main}: ${info.main.temp}&degF</p>`;
-    string +=    `<p>Air Pressure: ${info.main.pressure}</p>`;
-    string +=    `<p>Wind Speed: ${info.wind.speed}</p>`;
+    string +=    `<p><strong>Current Temperature:</strong> ${info.main.temp}&degF</p>`;
+    string +=    `<p><strong>Weather Conditions:</strong> ${info.weather[0].main}</p>`;
+    string +=    `<p><strong>Air Pressure:</strong> ${info.main.pressure}</p>`;
+    string +=    `<p><strong>Wind Speed:</strong> ${info.wind.speed}</p>`;
     string +=    `<button id="5day">5 Day Forecast</button>`;
     string +=  `</div>`;
     string += `</div>`;
